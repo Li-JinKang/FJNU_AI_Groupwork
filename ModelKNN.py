d7fb469642244978e5bdba86139f33623f19f52d
Li-JinKang <149137280+Li-JinKang@users.noreply.github.com>
2024-12-01 19:25:58 +0800

    Add files via upload

--- ModelKNN.py
+++ ModelKNN.py
@@ -8,12 +8,15 @@ data = pd.read_csv("data.csv")
 labels = data.iloc[:, 1:-1]
 target = data.iloc[:, -1]
 
-# 划分数据集
 x_train, x_test, y_train, y_test = train_test_split(labels, target, test_size=0.20)
 
+# 使训练数据均衡
+# Balance the training data.
 smote = SMOTE()
 x_train, y_train = smote.fit_resample(x_train, y_train)
 
+# 参数为经过GridSearchCV优化的最佳结果
+# The parameters are the best results obtained after optimizing with GridSearchCV.
 model = KNN(n_neighbors=2, metric='manhattan')
 model.fit(x_train, y_train)
 
